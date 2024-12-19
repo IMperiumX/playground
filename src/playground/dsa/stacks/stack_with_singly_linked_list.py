829144e413f153fd278c01b167634d1d7c593c81
IMperiumX <yusufadell.dev@gmail.com>
2024-12-19 22:57:00 +0200

    feat(stack): implement SinglyListStack using LinkedList

--- src/playground/dsa/stacks/stack_with_singly_linked_list.py
+++ src/playground/dsa/stacks/stack_with_singly_linked_list.py
@@ -1,2 +1,11 @@
+from playground.dsa import LinkedList, Node
+
+
+class SinglyListStack:
+    def __init__(self):
+        self._stack = LinkedList(Node(None))
+
+
 if __name__ == "__main__":
-    ...
+    ss = SinglyListStack()
+    print(ss)
