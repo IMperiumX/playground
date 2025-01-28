b35c599bc64e095562915dde7680430d19badd70
IMperiumX <yusufadell.dev@gmail.com>
2025-01-28 04:16:22 +0200

    feat(stack): implement push, pop, peek, is_empty, and size methods for SinglyListStack

--- src/playground/dsa/stacks/stack_with_singly_linked_list.py
+++ src/playground/dsa/stacks/stack_with_singly_linked_list.py
@@ -8,6 +8,21 @@ class SinglyListStack(StackADT):
     def __str__(self):
         return str(self._stack)
 
+    def push(self, item):
+        self._stack.push_front(item)
+
+    def pop(self):
+        return self._stack.pop_front()
+
+    def peek(self):
+        return self._stack.front()
+
+    def is_empty(self):
+        return self._stack.empty()
+
+    def size(self):
+        return self._stack.size()
+
 
 if __name__ == "__main__":
     ss = SinglyListStack()
