83a59a5e920496fd25ea4492f7d156c5840041c0
IMperiumX <yusufadell.dev@gmail.com>
2025-10-10 16:02:06 +0300

    feat(tree): add BinaryTree abstract class with methods for child retrieval, sibling identification, and inorder traversal

--- src/playground/dsa/trees/binary_tree.py
+++ src/playground/dsa/trees/binary_tree.py
@@ -1,3 +1,6 @@
+from playground.dsa.trees.tree_adt import Tree
+
+
 class Node:
     def __init__(self, data, left=None, right=None):
         self.data = data
@@ -158,3 +161,84 @@ if __name__ == "__main__":
     print("Is Full:", tree.is_full(root))
     print("Is Complete:", tree.is_complete(root, 0, tree.size(root)))
     print("Is Perfect:", tree.is_perfect(root, tree.height(root)))
+
+
+# Copyright 2013, Michael H. Goldwasser
+#
+# Developed for use with the book:
+#
+#    Data Structures and Algorithms in Python
+#    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+#    John Wiley & Sons, 2013
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class BinaryTree(Tree):
+    """Abstract base class representing a binary tree structure."""
+
+    # --------------------- additional abstract methods ---------------------
+    def left(self, p):
+        """Return a Position representing p's left child.
+
+        Return None if p does not have a left child.
+        """
+        raise NotImplementedError("must be implemented by subclass")
+
+    def right(self, p):
+        """Return a Position representing p's right child.
+
+        Return None if p does not have a right child.
+        """
+        raise NotImplementedError("must be implemented by subclass")
+
+    # ---------- concrete methods implemented in this class ----------
+    def sibling(self, p):
+        """Return a Position representing p's sibling (or None if no sibling)."""
+        parent = self.parent(p)
+        if parent is None:  # p must be the root
+            return None  # root has no sibling
+        else:
+            if p == self.left(parent):
+                return self.right(parent)  # possibly None
+            else:
+                return self.left(parent)  # possibly None
+
+    def children(self, p):
+        """Generate an iteration of Positions representing p's children."""
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
+
+    def inorder(self):
+        """Generate an inorder iteration of positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_inorder(self.root()):
+                yield p
+
+    def _subtree_inorder(self, p):
+        """Generate an inorder iteration of positions in subtree rooted at p."""
+        if self.left(p) is not None:  # if left child exists, traverse its subtree
+            for other in self._subtree_inorder(self.left(p)):
+                yield other
+        yield p  # visit p between its subtrees
+        if self.right(p) is not None:  # if right child exists, traverse its subtree
+            for other in self._subtree_inorder(self.right(p)):
+                yield other
+
+    # override inherited version to make inorder the default
+    def positions(self):
+        """Generate an iteration of the tree's positions."""
+        return self.inorder()  # make inorder the default
